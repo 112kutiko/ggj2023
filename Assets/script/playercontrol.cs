e487d17a0057c54e6f332858d9c66b650ec7942c
TheSpezX <114147789+TheSpezX@users.noreply.github.com>
2023-02-04 23:35:13 +0200

    ,

--- Assets/script/playercontrol.cs
+++ Assets/script/playercontrol.cs
@@ -25,16 +25,7 @@ public class playercontrol : NetworkBehaviour
     void awake()
     {
         controls = this;
- 
     }
-    // Start is called before the first frame update
-    void Start()
-    { 
-
-
-    }
-
-
 
     // Update is called once per frame
     void Update()
