eb9358b80bb847924d9edc63981b92edded4abeb
112kutiko <tadask04@gmail.com>
2023-02-04 23:40:39 +0200

    a

--- Assets/script/myBlock.cs
+++ Assets/script/myBlock.cs
@@ -8,8 +8,9 @@ public class myBlock : NetworkBehaviour
     public GameObject spawnerPoint;
     public bool isEnd, isfirst;
     public List<Mesh> pool1,pool0, poolEnd;
-    public bool isPlayer;  
-//    public NetworkVariable<int> clientId = new NetworkVariable<int>();
+    public bool isPlayer;
+    public int lygis = 0;
+        //    public NetworkVariable<int> clientId = new NetworkVariable<int>();
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class myBlock : NetworkBehaviour
         if (isPlayer) { return; }
         int ycount = Random.Range(0, poolEnd.Count);
         gameObject.GetComponent<MeshFilter>().mesh = poolEnd[ycount];
+
     }
 
     // Update is called once per frame
@@ -39,12 +41,14 @@ public class myBlock : NetworkBehaviour
                 int ycount = Random.Range(0, pool1.Count);
                 gameObject.GetComponent<MeshFilter>().mesh = pool1[ycount];
                 isEnd = false;
+                lygis = 1;
             }
             else if (isEnd == true && isfirst == true)
             {
                 int ycount = Random.Range(0, pool0.Count);
                 gameObject.GetComponent<MeshFilter>().mesh = pool0[ycount];
                 isEnd = false;
+                lygis = 2;
             }
 
             }
